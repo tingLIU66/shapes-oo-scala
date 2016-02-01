9087247b93e7158991d6798d9f4e20b003ffdfa1
Konstantin Läufer <laufer@cs.luc.edu>
2016-02-01 21:06:21 +0000

    added dummy bounding box implementation
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

---
+++ src/main/scala/boundingBox.scala
@@ -0,0 +1,9 @@
+package edu.luc.cs.laufer.cs372.shapes
+
+// TODO: implement this behavior
+
+object boundingBox {
+  def apply(s: Shape): Location = s match {
+    case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
+  }
+}
