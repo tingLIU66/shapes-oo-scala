6db249a66fc90b1ac0ab40eebecfa80c6fecc6d3
TingLiu6 <tliu6@luc.edu>
2018-02-27 00:05:42 -0600

    Added missing classes in shapes.scala

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -5,11 +5,26 @@ package edu.luc.cs.laufer.cs372.shapes
   */
 sealed trait Shape
 
+case class Ellipse(semimajoraxis:Int, semiminoraxis:Int) extends Shape{
+  require(semimajoraxis >= 0, "invalid major axis")
+  require(semiminoraxis >= 0, "invalid minor axis")
+
+}
+
 case class Rectangle(width: Int, height: Int) extends Shape
 
 case class Location(x: Int, y: Int, shape: Shape) extends Shape {
   require(shape != null, "null shape in location")
 }
+
+case class Group(shape : Shape*) extends Shape{
+  require(shape != null, "null shape in group")
+
+}
+
+
+
+
 
 // TODO add missing case classes (see test fixtures)
 // TODO must include validity checking for constructor arguments
