c3fce7d8e2fa882e905d75530dffce9fe5ba1d6c
TingLiu6 <tliu6@luc.edu>
2018-03-02 15:39:25 -0600

    Added missing classes in shapes.scala Added rectangle boundingbox

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -4,6 +4,46 @@ package edu.luc.cs.laufer.cs372.shapes
 
 object boundingBox {
   def apply(s: Shape): Location = s match {
-    case Rectangle(width: Int, height: Int) => Location(0, 0, Rectangle(width, height)) // not yet implemented
+
+    case Ellipse(semimajoraxis: Int, semiminoraxis: Int) => Location(-semimajoraxis, -semiminoraxis, Rectangle(semimajoraxis * 2, semiminoraxis * 2))
+    case Rectangle(width: Int, height: Int)              => Location(0, 0, Rectangle(width, height))
+    case Location(x: Int, y: Int, shape: Shape)          => Location(x + apply(shape).x, y + apply(shape).y, apply(shape).shape)
+
+    case Group(shapes @ _*)                              => calgroupboundingbox(Group(shapes: _*))
+
   }
+
+  def calgroupboundingbox(shapes: Group): Location = {
+
+    //var boundboxlist = List.empty
+
+    var xmin = 0
+    var xmax = 0
+    var ymin = 0
+    var ymax = 0
+    var xlist = List.empty
+    var ylist = List.empty
+    var xmaxlist = List.empty
+    var ymaxlist = List.empty
+
+    for (shape <- shapes) {
+
+      var rec = apply(shape).shape.asInstanceOf[Rectangle]
+
+      xlist :+ apply(shape).x
+      ylist :+ apply(shape).y
+      xmaxlist :+ apply(shape).x + rec.width
+      ymaxlist :+ apply(shape).y + rec.height
+    }
+
+    xmax = xmaxlist.max
+    xmin = xlist.min
+
+    ymax = ymaxlist.max
+    ymin = ylist.min
+
+    Location(xmin, ymin, Rectangle(xmax - xmin, ymax - ymin))
+
+  }
+
 }
